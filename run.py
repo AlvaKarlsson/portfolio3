23085371c1b58e8caca1bb65255472d3e243a262
Alva Karlsson <alva_karlsson@live.se>
2022-11-24 08:23:39 +0000

    wrote first function that starts the game and writes out a welcome message with the rules and input description

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 from random import randint
 
-alternatives = ['Rock', 'Paper', 'Scissors']
+alternatives = ['r', 'p', 's']
+#r = Rock, p = Paper, s = Scissors
 
 computer = alternatives[randint(0, 2)]
+
+def start_game():
+    '''
+    Welcome message so the player knows the rules, how to input their choise
+    and where to write the input
+    '''
+    print('Hi and welcome to "Rock, Paper, Scissors"!\n')
+    print('The rules are:')
+    print('Rock beats Scissors, Scissors beats Paper and Paper beats Rock\n')
+    print('Input should be: r = rock, s = scissors, p = paper')
+
+    players_choise = input('Write your choise here:\n')
+
+start_game()

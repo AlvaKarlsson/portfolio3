b351e3caf7ff55f501c97b3c854b4d3a17ddb397
Alva Karlsson <alva_karlsson@live.se>
2022-11-24 09:02:44 +0000

    wrote a while loop that prints out if the computer wins, the player wins or it's a tie

--- run.py
+++ run.py
@@ -5,16 +5,31 @@ alternatives = ['r', 'p', 's']
 
 computer = alternatives[randint(0, 2)]
 
-def start_game():
-    '''
-    Welcome message so the player knows the rules, how to input their choise
-    and where to write the input
-    '''
-    print('Hi and welcome to "Rock, Paper, Scissors"!\n')
-    print('The rules are:')
-    print('Rock beats Scissors, Scissors beats Paper and Paper beats Rock\n')
-    print('Input should be: r = rock, s = scissors, p = paper')
+player = False
 
-    players_choise = input('Write your choise here:\n')
+print('Hi and welcome to "Rock, Paper, Scissors"!\n')
+print('The rules are:')
+print('Rock beats Scissors, Scissors beats Paper and Paper beats Rock\n')
+print('Input should be: r = rock, s = scissors, p = paper')
 
-start_game()
+while player is False:
+    player = input('Write your choise here:\n')
+    if player == computer:
+        print("That's a tie!")
+    elif player == 'r':
+        if computer == 'p':
+            print("Oh, you lost...")
+        else:
+            print("You win!")
+    elif player == 'p':
+        if computer == 's':
+            print("Oh, you lost...")
+        else:
+            print("You win!")
+    elif player == 's':
+        if computer == 'r':
+            print("Oh, you lost...")
+        else:
+            print("You win!")
+    else:
+        print("The input is not valid")
